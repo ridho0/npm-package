5260cd46a643a18caaafc2b7cc6c6ded2117f61d
ridho0 <ridho230994@gmail.com>
2017-05-17 14:50:25 +0700

    err fixed

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // exports.toMataUang = function(matauang, nominal) {
-let to = {}
-to.rupiah = function(nominal) {
-  arr = nominal.split('').reverse()
-  let tmp = []
-  for (let i = 0; i < arr.length; i++){
-    tmp.push(arr[i])
-      if((i+1) % 3 == 0){
+let toMataUang = function(matauang, nominal) {
+// let to = {}
+// to.rupiah = function(nominal) {
+  if(matauang.toLowerCase() == "rupiah"){
+    arr = nominal.split('').reverse()
+    let tmp = []
+    for (let i = 0; i < arr.length; i++){
+      tmp.push(arr[i])
+      if((i+1) % 3 == 0 && nominal[i+1]){
         tmp.push('.')
       }
+    }
+    return `Rp. ${tmp.reverse().join('')},00`
   }
-  return `Rp. ${tmp.reverse().join('')},00`
 }
 
-exports.to
+// exports.to
+console.log(toMataUang('rupiah', '100000000'));
