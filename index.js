238dc0b7598ebf55d4f36b96c21547cf57f27906
ridho0 <ridho230994@gmail.com>
2017-05-17 16:03:48 +0700

    nyoba

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 // exports.toMataUang = function(matauang, nominal) {
-let toMataUang = function(matauang, nominal) {
-// let to = {}
-// to.rupiah = function(nominal) {
-  if(matauang.toLowerCase() == "rupiah"){
-    arr = nominal.split('').reverse()
-    let tmp = []
-    for (let i = 0; i < arr.length; i++){
-      tmp.push(arr[i])
-      if((i+1) % 3 == 0 && nominal[i+1]){
-        tmp.push('.')
-      }
-    }
-    return `Rp. ${tmp.reverse().join('')},00`
-  }
-}
+// // let toMataUang = function(matauang, nominal) {
+//   if(matauang.toLowerCase() == "rupiah"){
+//     arr = nominal.split('').reverse()
+//     let tmp = []
+//     for (let i = 0; i < arr.length; i++){
+//       tmp.push(arr[i])
+//       if((i+1) % 3 == 0 && nominal[i+1]){
+//         tmp.push('.')
+//       }
+//     }
+//     return `Rp. ${tmp.reverse().join('')},00`
+//   }
+// }
 
-// exports.to
-console.log(toMataUang('rupiah', '100000000'));
+let to = {}
+to.rupiah = function(nominal) {
+  arr = nominal.split('').reverse()
+      let tmp = []
+      for (let i = 0; i < arr.length; i++){
+        tmp.push(arr[i])
+        if((i+1) % 3 == 0 && nominal[i+1]){
+          tmp.push('.')
+        }
+      }
+      return `Rp. ${tmp.reverse().join('')},00`
+}
+exports.to
+// console.log(toMataUang('rupiah', '100000000'));
+// console.log(to.rupiah('10000'));
